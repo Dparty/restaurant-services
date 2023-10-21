866c136ee9494bd3c7b759a0f0789f41c4046781
Dan Chen <dan.chen@boardware.com>
2023-10-21 16:46:01 +0800

    feat

--- models/restaurant.go
+++ models/restaurant.go
@@ -84,12 +84,11 @@ func (r Restaurant) CreateItem(name string, pricing int64, attributes restaurant
 		Tags:         tags,
 		Printers:     printers,
 	}
-	db.Save(&i)
-	// item, err := itemRepository.Save(&i)
-	// if err != nil {
-	// 	return Item{}, err
-	// }
-	return NewItem(i), nil
+	item, err := itemRepository.Save(&i)
+	if err != nil {
+		return Item{}, err
+	}
+	return NewItem(*item), nil
 }
 
 func (r Restaurant) CreateTable(label string, x, y int64) (Table, error) {
